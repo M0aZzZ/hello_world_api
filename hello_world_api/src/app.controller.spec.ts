976272ac45437a0a9d6d4fa8928b3c7e0a603904
m0az <moazkm2@gmail.com>
2026-04-09 15:59:45 +0300

    add testing fille by jest

---
+++ hello_world_api/src/app.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+    }).compile();
+
+    appController = module.get<AppController>(AppController);
+  });
+
+  it('should return "hello world !" when called', () => {
+    expect(appController.getHelloWorld()).toBe('hello world !');
+  });
+});
